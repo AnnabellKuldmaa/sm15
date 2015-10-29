8edf585d87edf80801d199ddaffe1f79756df3e0
AnnabellKuldmaa <annabell.kuldmaa@gmail.com>
2015-10-29 23:02:25 +0200

    #12 Äkki teeme nii?

--- ConformanceChecker/src/domain/Boundary.java
+++ ConformanceChecker/src/domain/Boundary.java
@@ -1,14 +1,26 @@
 package domain;
 
-import domain.Controller;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Boundary {
 
 	public static void main(String[] args) {
-		
-		Controller controller = new Controller();
-		controller.doLogReplay("test.pnml", "test.xes");
-			
+		BufferedReader bufferReader = new BufferedReader(new InputStreamReader(
+				System.in));
+		String xPathOfLog;
+		try {
+			System.out.println("Insert xPath of Petri Net (.pnml file)");
+			String xPathOfPetriNet = bufferReader.readLine();
+			System.out.println("Insert xPath of Log (.xes file)");
+			xPathOfLog = bufferReader.readLine();
+			Controller controller = new Controller();
+			controller.doLogReplay(xPathOfPetriNet, xPathOfLog);
+		} catch (Exception e) {
+			System.out.println("Invalid input");
+			// e.printStackTrace();
+		}
+
 	}
 
 }
